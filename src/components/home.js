5c25b2c5ccb707ceff5a06d0f9408d2aa4e7aba0
Ben Governali <bgovern93@gmail.com>
2019-07-24 12:08:11 -0400

    added button to homepage for testing library pull from spotify

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  onClick() {
+    fetch("http://localhost:3000/auth/scan");
+  }
+
   render() {
     return (
       <>
         <h1>This is the homepage</h1>
+        <button onClick={this.onClick}>Scan Spotify</button>
       </>
     );
   }
