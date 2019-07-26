5ef33e9109e70832845271675471a1f9d00a75e0
Ben Governali <bgovern93@gmail.com>
2019-07-26 14:25:01 -0400

    replaces scan button with form for user to submit a band tto search

--- src/components/home.js
+++ src/components/home.js
@@ -2,25 +2,51 @@ import React, { Component } from "react";
 
 class Home extends Component {
   state = {
-    bearer: ""
+    token: "",
+    artist: ""
   };
 
   async componentDidMount() {
     const token = this.props.location.state.bearer;
     await this.setState({
-      bearer: token
+      token: token
     });
   }
 
+  /*
   onClick = () => {
     fetch(`http://localhost:3000/home/scan/${this.state.bearer}`);
+  };
+  */
+
+  handleArtistChange = e => {
+    this.setState({
+      artist: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const artist = this.state.artist;
+    const token = this.state.token;
+    const url = `http://localhost:3000/home/scan/${token}/${artist}`;
+    fetch(url);
   };
 
   render() {
     return (
       <>
         <h1>This is the homepage</h1>
-        <button onClick={this.onClick}>Scan Spotify</button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Please Enter an Artist"
+            onChange={this.handleArtistChange}
+            name="title"
+            value={this.state.title}
+          />
+          <input type="submit" value="Submit" />
+        </form>
       </>
     );
   }
